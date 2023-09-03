313a400f7689eb4a00d9deab787804888c069cc6
liam9930 <liam9930@gmail.com>
2023-09-03 21:54:07 +0800

    修改index頁面

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+
+const swiper = new Swiper(".mySwiper", {
+    direction: "horizontal",
+    grabCursor: true,
+    loop: true, // 設置為循環播放
+    slidesPerView: "auto",
+    preloadImages: true,
+    autoplay: {
+        delay: 0, // 自動播放間隔時間（單位：毫秒）
+        disableOnInteraction: false
+    },
+    lazy: {
+        loadPrevNext: true
+    },
+    speed: 70000 // 滑動速度（單位：毫秒）
+});
