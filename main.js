3cadc07bff8f3623e95077b84673de8fd11dcde4
liam9930 <liam9930@gmail.com>
2023-09-10 17:25:42 +0800

    新增login頁面,index樣式

--- main.js
+++ main.js
@@ -3,18 +3,11 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
 
-const swiper = new Swiper(".mySwiper", {
-    direction: "horizontal",
-    grabCursor: true,
-    loop: true, // 設置為循環播放
-    slidesPerView: "auto",
-    preloadImages: true,
-    autoplay: {
-        delay: 0, // 自動播放間隔時間（單位：毫秒）
-        disableOnInteraction: false
-    },
-    lazy: {
-        loadPrevNext: true
-    },
-    speed: 70000 // 滑動速度（單位：毫秒）
+
+$(".needs-validation").each(function (index) {
+    $(this).on("submit", function (e) {
+      e.preventDefault();
+      $(this).addClass("was-validated");
+    });
 });
+  
